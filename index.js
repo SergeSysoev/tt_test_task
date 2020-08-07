cd336efaaae95af401e294a53f8de85a18ab7b84
Sergey Sysoev <ssysoev@bekitzur.com>
2020-08-07 12:32:07 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const arrayToString = (numbersArray) => {
+    if (numbersArray.length === 1) {
+        return numbersArray[0].toString();
+    }
+
+    let result = numbersArray[0];
+
+    //
+
+    return result;
+};
+
+exports.arrayToString = arrayToString;
